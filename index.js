d5807925b715bb936e53a5de41374a4d30f1dcf3
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-06 15:10:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ const returnLastTwoDrivers = function(drivers){
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
 
-const createFareMultiplier
+const createFareMultiplier(int)
